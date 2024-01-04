014892cd39b952f25cc2e13d9de37214124b0e7a
osm <osm0822@naver.com>
2024-01-05 00:02:31 +0900

    JPA 기본 키 생성 전략의 변경에 따라 초기화 스크립트 업데이트

--- mysql/sqls/2.initdata.sql
+++ mysql/sqls/2.initdata.sql
@@ -1,6 +1,5 @@
-INSERT INTO category(category_id, name) values(1, "문화/예술");
-INSERT INTO category(category_id, name) values(2, "공연전시/행사");
-INSERT INTO category(category_id, name) values(3, "식품/음료");
-INSERT INTO category(category_id, name) values(4, "교육/체험");
-INSERT INTO category(category_id, name) values(5, "스포츠/레저");
-UPDATE hibernate_sequence SET next_val=10;
+INSERT INTO category(category_id, name) values(NULL, "문화/예술");
+INSERT INTO category(category_id, name) values(NULL, "공연전시/행사");
+INSERT INTO category(category_id, name) values(NULL, "식품/음료");
+INSERT INTO category(category_id, name) values(NULL, "교육/체험");
+INSERT INTO category(category_id, name) values(NULL, "스포츠/레저");
