a5b76d20d88e75e5398393869a18b550bd12513f
osm <osm0822@naver.com>
2024-01-04 20:39:52 +0900

    mysql dockerfile 추가

---
+++ mysql/sqls/2.initdata.sql
@@ -0,0 +1,6 @@
+INSERT INTO category(category_id, name) values(1, "문화/예술");
+INSERT INTO category(category_id, name) values(2, "공연전시/행사");
+INSERT INTO category(category_id, name) values(3, "식품/음료");
+INSERT INTO category(category_id, name) values(4, "교육/체험");
+INSERT INTO category(category_id, name) values(5, "스포츠/레저");
+UPDATE hibernate_sequence SET next_val=10;
